3968fec88acfc3bd22ebca0e60804ca271f5c36a
Essien Joseph Divine <150144617+joeswrld@users.noreply.github.com>
2026-09-19 03:09:30 +0100

    Update index.ts

---
+++ packages /core /src/index.ts
@@ -0,0 +1,11 @@
+// Already implemented
+export * from "./consensus-params";
+export * from "./fees";
+
+// Phase 1 — core data structures
+export * from "./bytes";
+export * from "./hash";
+export * from "./target";
+export * from "./transaction";
+export * from "./merkle";
+export * from "./block";
